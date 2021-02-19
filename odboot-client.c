5599bbfb4d6bf828e496d7a6cefc95b0168a2d08
Paul Cercueil <paul@crapouillou.net>
2021-02-19 16:08:34 +0000

    client: Add support for the LDK (horizontal)
    Signed-off-by: Paul Cercueil <paul@crapouillou.net>

--- odboot-client.c
+++ odboot-client.c
@@ -73,6 +73,7 @@ static const struct board lepus_boards[] = {
 	{ "rs97", "lepus", "Anbernic RS-97 v2.0" },
 	{ "rg300", "lepus", "Anbernic RG-300 non-IPS" },
 	{ "ldkv", "lepus", "LDK (vertical)" },
+	{ "ldkh", "lepus", "LDK (horizontal)" },
 };
 
 static const struct board_group groups[] = {
