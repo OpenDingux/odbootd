0385a5e70f87048db8d0bfb9e5e0eb98930733fd
Paul Cercueil <paul@crapouillou.net>
2021-04-05 13:38:31 +0100

    Fix wrong table entries for the rs90
    Signed-off-by: Paul Cercueil <paul@crapouillou.net>

--- odboot-client.c
+++ odboot-client.c
@@ -65,9 +65,9 @@ static const struct board gcw0_boards[] = {
 };
 
 static const struct board rs90_boards[] = {
-	{ "rs90", "rs90_v21", "Anbernic RS-90 v2.1" },
-	{ "rs90", "rs90_v30", "Anbernic RS-90 v3.0" },
-	{ "rg99", "rs90_v21", "Anbernic RG-99" },
+	{ "rs90", "v21", "Anbernic RS-90 v2.1" },
+	{ "rs90", "v30", "Anbernic RS-90 v3.0" },
+	{ "rg99", "v21", "Anbernic RG-99" },
 };
 
 static const struct board lepus_boards[] = {
